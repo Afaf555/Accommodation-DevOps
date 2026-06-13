4dd9980b4d3604ae8598cf6436b0828534603204
Anastasija Frangova <anastasija.frangova@students.finki.ukim.mk>
2026-06-13 15:29:57 +0200

    Allow frontend.local origin for CORS in k8s

--- lab_emt/src/main/java/mk/ukim/finki/wp/lab_emt/config/JwtWebSecurityConfig.java
+++ lab_emt/src/main/java/mk/ukim/finki/wp/lab_emt/config/JwtWebSecurityConfig.java
@@ -81,7 +81,7 @@ public class JwtWebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000",
-                "http://frontend.local"));
+                "http://frontend.local:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
